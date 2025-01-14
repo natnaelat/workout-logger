c0ffa8677cb81c04071cb8f2d7ec56a6a5fe4827
natnaelat <natnaelt@vt.edu>
2025-01-14 14:11:43 -0500

    react package download

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { AuthProvider } from "react-oidc-context";
-import App from "./App";
+// index.js
 import "./index.css";
+import { AuthProvider } from "react-oidc-context";
 
-// OIDC configuration for Cognito
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_AlNPjKJtj",
   client_id: "6961qcc5ik6qg1rt0ucpa5vp8p",
   redirect_uri: "https://main.d2fzktvpiprrqk.amplifyapp.com/",
   response_type: "code",
-  scope: "phone openid email",
+  scope: "email openid phone",
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// Wrap the application with AuthProvider to provide authentication context
+// wrap the application with AuthProvider
 root.render(
   <React.StrictMode>
     <AuthProvider {...cognitoAuthConfig}>
