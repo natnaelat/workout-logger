f929a386b607482e28400a91e7365f73d7bb12d2
natnaelat <natnaelt@vt.edu>
2025-01-14 19:47:48 -0500

    update redirect_uri

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from "./App"; // Import your App component
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_4KSxWHrdJ",
   client_id: "12q7uj83n3hppb93ram0s5t6bk",
-  redirect_uri: "https://example.com",
+  redirect_uri: "https://main.d2fzktvpiprrqk.amplifyapp.com/",
   response_type: "code",
   scope: "aws.cognito.signin.user.admin email openid phone profile",
 };
