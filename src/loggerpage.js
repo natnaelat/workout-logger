fc0112b461b5f8ae786637bd5babb144b5653271
natnaelat <natnaelt@vt.edu>
2025-03-02 18:23:11 -0500

    change back

--- src/loggerpage.js
+++ src/loggerpage.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import "./loggerpage.css";
-
+//backs
 const LogPage = () => {
   const location = useLocation();
   const exerciseName =
